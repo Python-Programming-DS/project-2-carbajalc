5eb5742a1d3be772900f832acbe0bc07958d0f33
carbajalc <101572082+carbajalc@users.noreply.github.com>
2025-11-07 10:17:28 -0500

    Update CC_Project2_PartC.py

--- CC_Project2_PartC.py
+++ CC_Project2_PartC.py
@@ -1,1 +1,24 @@
+    # Uses trained model to get the best position on current board
+    def MLturn(self, board, best_nn):
+        board = self.board.board
+        new_board = [[]] # need the correct shape for model
 
+        # Converting board into 1, -1 and 0 format
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "X":
+                    new_board[0].append(1)
+                elif board[i][j] == "O":
+                    new_board[0].append(-1)
+                else:
+                    new_board[0].append(0)
+        new_board = pd.DataFrame(new_board)
+
+        best_pos = int(best_nn.predict(new_board)[0])
+
+        if best_pos < 3:
+            return [0, best_pos]
+        elif best_pos < 6:
+            return [1, best_pos - 3]
+        else:
+            return [2, best_pos - 6]
