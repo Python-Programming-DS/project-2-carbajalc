e0ba69af151f9e941cd68818119717715f6b4fd6
carbajalc <101572082+carbajalc@users.noreply.github.com>
2025-11-07 10:18:01 -0500

    Update CC_Project2_PartC.py

--- CC_Project2_PartC.py
+++ CC_Project2_PartC.py
@@ -22,3 +22,49 @@
             return [1, best_pos - 3]
         else:
             return [2, best_pos - 6]
+
+ # plays computer game using machine learning    
+    def playMLGame(self):
+        # Building and training model to be used in other functions, so we do not have to repeatedly build a new model each time
+        warnings.filterwarnings('ignore')
+        tictac_data = pd.read_csv('tictac_single.txt', sep = r'\s+', header = None) # No header, separated by white space instead
+
+        # Splitting into test and train sets (model takes too long to train on all data)
+        y = tictac_data[[9]]
+        X = tictac_data.drop(columns = 9)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+        
+        nn_model = MLPClassifier()
+
+        # Finding best params for NN
+        param_grid = {'hidden_layer_sizes': [100, 150],
+                  'activation': ['identity', 'logistic', 'relu'],
+                  'solver': ['lbfgs', 'sgd', 'adam']}
+
+        nn_model = GridSearchCV(nn_model, param_grid)
+        nn_model.fit(X_train, y_train)
+
+        best_nn = nn_model.best_estimator_
+
+        print("New Game: X goes first.")
+        print()
+        self.board.printBoard()
+        play = True
+
+        while play == True:
+            if self.turn == 'X':
+                # Validating entry
+                entry = False
+                while entry == False:
+                    entry = self.validateEntry()
+                print("Thank you for your selection.")
+            else:
+                move = self.MLturn(self.board.board, best_nn)
+                self.board.board[move[0]][move[1]] = self.turn
+
+            if self.checkEnd() == True:
+                 user = input("Another game? Enter Y or y for yes.\n").lower()
+                 play = False
+                 return user
+
+            self.switchPlayer()
